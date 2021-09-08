f6423f994c6c22878a6b985f4a0e338a2d95f736
Marcus Reynolds <marcusroy93@gmail.com>
2021-09-08 09:16:31 -0700

    added event listener for buttons to record score

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # javascriptQuiz
-This is a web page that will present the user with a timed quiz  about JavaScript, then save and rank their score
+This is a web page that will present the user with a timed quiz  about JavaScript. The user will then click buttons to answer a series of prompts.  scoring correctly will give the user a point and then record it.  Scoring incorrectly will give no  points.  After the quiz is completed or the timer runs out, it will then display the score of the user and record it on a list of top scores.
--- script.js
+++ script.js
@@ -1,5 +1,55 @@
+const quizQuestions = [
+    {
+        questionOne: 'Which of these is NOT a reserved word?',
+        1: 'goto',
+        2: 'native',
+        3: 'delete',
+        4: 'into',
+    },
+    {
+        questionTwo: 'the Date() function returns which of the following:',
+        1: 'object',
+        2: 'boolean',
+        3: 'string',
+        4: 'interger',
+    },
+    {
+        questionThree: 'What character do you use to write single line comments in JavaScript?',
+        1: '--',
+        2: '//',
+        3: '~~',
+        4: '``',
+    },
+    {
+        questionFour: 'Which company developed Javascript?',
+        1: 'Oracle',
+        2: 'Bell Laboratories',
+        3: 'Microsoft',
+        4: 'Netscape'
+    }
+];
+const correctAnswers = ['into','string','//','Netscape'];
+const quizButtons = document.getElementsByClassName('quizAnswerButton');
+const quizPopup = document.getElementById('answerNotification');
+
+let currentQuestion = 0;
+let finalScore = 0;
+
+loadQuestions(currentQuestion);
+
+quizButtons.addEventListener("click", () => {
+    if (correctAnswers.some(quizButton.value) === true) {
+        finalScore += 10;
+        quizPopup.value = 'Correct!';
+    } else {
+        quizPopup.value = 'Incorrect!';
+    }
+    currentQuestion += 1
+    loadQuestions(currentQuestion);
+});     
+
 function startTimer(duration, display) {
-    var timer = duration, seconds;
+    let timer = duration, seconds;
     setInterval(function () {
         seconds = parseInt(timer , 10);
 
@@ -9,7 +59,7 @@ function startTimer(duration, display) {
             timer = duration;
         }
     }, 1000);
-}
+};
 
 window.onload = function () {
     const twoMinutes = 120,
